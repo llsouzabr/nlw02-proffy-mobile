8dce633c7a3625135861e6fb21db75f484e739c1
Leonardo Souza <llsouza.br@gmail.com>
2020-08-06 12:35:30 -0300

    Creating Study Pages

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -19,6 +19,10 @@ function Landing() {
         navigate('GiveClasses');
     }
 
+    function handleNavigateToStudyPages() {
+        navigate('Study');
+    }
+
     return (
         <View style={styles.container}>
             <Image source={landingImg} style={styles.banner} />
@@ -29,7 +33,10 @@ function Landing() {
             </Text>
 
             <View style={styles.buttonsContainer}>
-                <RectButton style={[styles.button, styles.buttonPrimary]}>
+                <RectButton
+                    style={[styles.button, styles.buttonPrimary]}
+                    onPress={handleNavigateToStudyPages}
+                >
                     <Image source={studyIcon} />
                     <Text style={styles.buttonText}>Estudar</Text>
                 </RectButton>
---
+++ src/pages/TeacherList/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+import { Archivo_700Bold } from '@expo-google-fonts/archivo';
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor:'#8257e5',
+        flex: 1,
+        justifyContent: 'center',
+        padding:40
+    },
+
+});
+
+export default styles;
